af36999b6aeaf37192cec3fe54fc7efce5e3fb91
Dhanushnetha <dk40828@gmail.com>
2024-12-24 11:41:27 +0530

    property Id changes

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -31,7 +31,7 @@ async function PropertyDetailsPage({params}:{params:{id:string}}) {
         <header className="flex justify-between items-center mt-4">
             <h1 className="text-4xl font-bold capitalize">{property.tagline}</h1>
             <div className="flex items-center gap-x-4">
-                <ShareButton name={property.name} propertyId={property.profileId} />
+                <ShareButton name={property.name} propertyId={property.id} />
                 <FavoriteToogleButton propertyId={property.id} />
             </div>
         </header>
