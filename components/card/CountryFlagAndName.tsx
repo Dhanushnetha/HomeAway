7e879fdf0f1b6da510d520173532a914f101ef00
Dhanushnetha <dk40828@gmail.com>
2024-12-28 01:52:20 +0530

    spacing issue

--- components/card/CountryFlagAndName.tsx
+++ components/card/CountryFlagAndName.tsx
@@ -4,7 +4,7 @@ function CountryFlagAndName({countryCode} : {countryCode: string}) {
     const validCountry = findCountryByCode(countryCode)!;
     const countryName = validCountry.name.length > 20 ? `${validCountry?.name.substring(0,20)}...` : validCountry?.name;
   return (
-    <span className="flex justify-between items-center gap-2 text-sm">
+    <span className="flex items-center gap-2 text-sm">
         <Flag code={countryCode} style={{ width: 20, height: 20 }} /> {countryName}
     </span>
   )
